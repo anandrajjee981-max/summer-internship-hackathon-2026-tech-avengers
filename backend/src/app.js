a1095346cbb98e2c4977f3fe0fc697f2e90346eb
anand raj <git add README.md>
2026-05-09 12:19:31 +0530

    debug

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 const cookieparser = require("cookie-parser")
 app.use(express.json())
 app.use(cookieparser())
+app.use(cors())
 const userroutes = require('../src/routes/user.routes')
 app.use("/api/auth",userroutes)
 
