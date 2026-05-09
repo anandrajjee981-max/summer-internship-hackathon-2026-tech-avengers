c486635ab62e633d6baaf2bbc8de1e6bed0856d7
anand raj <git add README.md>
2026-05-09 12:29:00 +0530

    fix-cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,24 @@
 const express = require("express")
 const app = express()
+
 const cors = require("cors")
 const cookieparser = require("cookie-parser")
+
 app.use(express.json())
+
 app.use(cookieparser())
-app.use(cors())
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
+app.get("/", (req,res)=>{
+    res.send("API running")
+})
+
 const userroutes = require('../src/routes/user.routes')
+
 app.use("/api/auth",userroutes)
 
 module.exports = app
